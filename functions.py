58ed96d9c7722b7d5c13f494adf6f99de836b106
AronDJacobsen <59569696+AronDJacobsen@users.noreply.github.com>
2022-11-19 11:39:43 +0100

    update

--- functions.py
+++ functions.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def posterior(p, q, a, b):
-    numerator = p * (q**a) * ((1-q)**b)
-    denominator = p * (q**a) * ((1-q)**b) + (1-p) * ((1-q)**a) * (q**b)
+    numerator = p * ( (q**a) * ((1-q)**b) )
+    denominator = numerator + (1-p) * ( ((1-q)**a) * (q**b) )
     posterior_prob = numerator / denominator
     return posterior_prob
 
@@ -22,26 +22,41 @@ def run_simulation(n_simutations, n_agents, prior, signal_accuracy):
         results[sim]['ab'] = np.zeros(n_agents)
         a = 0
         b = 0
-        for idx, agent in enumerate(range(n_agents)):
+        for agent in range(n_agents):
 
             marble = np.random.binomial(1, signal_accuracy, size=None) # 2/3
-            if marble == 1:
+
+            if marble == 1: # if e.g. the blue marble
                 posterior_prob = posterior(prior, signal_accuracy, a+1, b)
-            else:
+            else: # if e.g. the red marble
                 posterior_prob = posterior(prior, signal_accuracy, a, b+1)
 
-            if (posterior_prob >= prior):
+            # if posterior is larger than the prior
+            if (posterior_prob > prior):
                 a += 1
-                results[sim]['a'][idx] = 1
-                results[sim]['ab'][idx] = 1
+                results[sim]['a'][agent] = 1
+                results[sim]['ab'][agent] = 1
+
+            # if they are the same
+            elif posterior_prob == prior:
+                # the choice is 50/50
+                #  choice = np.random.binomial(1, 1/2, size=None)
+                choice = marble
+                if choice == 1:
+                    a += 1
+                    results[sim]['a'][agent] = 1
+                    results[sim]['ab'][agent] = 1
+                else:
+                    b += 1
+                    results[sim]['ab'][agent] = -1
+
+            # if less than prior
             else:
                 b += 1
-                results[sim]['ab'][idx] = -1
+                results[sim]['ab'][agent] = -1
 
         results[sim]['a_cumsum'] = np.cumsum(results[sim]['a'])
         results[sim]['ab_cumsum'] = np.cumsum(results[sim]['ab'])
-
-        #print("Simulation", sim, "marble was")
 
     return results
 
--- main.py
+++ main.py
@@ -9,13 +9,14 @@ n_agents = 100
 
 prior = 1/2
 
-signal_accuracy = 1/2
+signal_accuracy = 0.505
 
 
 results = run_simulation(n_simutations, n_agents, prior, signal_accuracy)
 
 for sim in range(n_simutations):
     plt.plot(results[sim]['a_cumsum'])
+    plt.title(f"Signal accuracy = {signal_accuracy}")
 
 plt.show()
 
